74b3167dd1d2b9296010f2bc9f8be84d0c7ddd23
amalie-overgaard <amalie-overgaard@live.dk>
2017-12-20 02:17:25 +0100

    Update db_con.php

--- db_con.php
+++ db_con.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-$DB_HOST = 'amalieovergaard.dk.mysql';
-$DB_USER = 'amalieovergaard_dk_kraglund';
-$DB_PASS = 'kraglund22';
-$DB_NAME = 'amalieovergaard_dk_kraglund';
+$DB_HOST = '';
+$DB_USER = '';
+$DB_PASS = '';
+$DB_NAME = '';
 // $DB_PORT = '8889';
 $link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 //$link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
